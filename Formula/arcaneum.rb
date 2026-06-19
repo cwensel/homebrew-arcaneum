d0ed216fe705d1ac0d0e7e7f3e5b3085f347d8c1
cwensel <>
2026-06-19 02:27:13 +0000

    Update arcaneum to v0.8.2

--- Formula/arcaneum.rb
+++ Formula/arcaneum.rb
@@ -7,8 +7,8 @@ class Arcaneum < Formula
 
   desc "CLI tools for semantic and full-text search with Qdrant and MeiliSearch"
   homepage "https://github.com/cwensel/arcaneum"
-  url "https://github.com/cwensel/arcaneum/releases/download/v0.8.1/arcaneum-0.8.1.tar.gz"
-  sha256 "22d75536a87a91ff6b27bbb0feb04289cd656e5190a7529ad5f22ccb93df0669"
+  url "https://github.com/cwensel/arcaneum/releases/download/v0.8.2/arcaneum-0.8.2.tar.gz"
+  sha256 "e19afbe91de427ee6f8d1dc46dc4d9e26b61b0aa692b643443b0c24ae224dfa3"
   license "MIT"
 
   depends_on "python@3.12"
